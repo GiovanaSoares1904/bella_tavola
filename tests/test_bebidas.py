50878e53efaa928355f9e00f07172b808880da06
GiovanaSoares1904 <giovanasoares_1804@outlook.com.br>
2026-05-05 15:09:17 -0300

    Refatorando

--- tests/test_bebidas.py
+++ tests/test_bebidas.py
@@ -9,11 +9,11 @@ def test_listar_bebidas_retorna_200():
     response = client.get("/bebidas")
     assert response.status_code == 200
 
-# def test_listar_bebidas_retorna_lista():
-#     response = client.get("/bebidas")
-#     dados = response.json()
-#     assert "bebidas" in dados
-#     assert isinstance(dados["bebidas"], list)
+def test_listar_bebidas_retorna_lista():
+     response = client.get("/bebidas")
+     dados = response.json()
+     assert "bebidas" in dados
+     assert isinstance(dados["bebidas"], list)
 
 def test_listar_bebidas_retorna_pelo_menos_uma_bebida():
     response = client.get("/bebidas")
